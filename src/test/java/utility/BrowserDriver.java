d728578329ecc75ff223c8cad4cae0e3b107c232
jmsdaq <jamesdaquioag6@gmail.com>
2024-08-30 17:22:36 +0800

    refactor: implement WebDriver setup/teardown in Cucumber Hooks

--- src/test/java/utility/BrowserDriver.java
+++ src/test/java/utility/BrowserDriver.java
@@ -1,21 +1,28 @@
-package utility;
-
+// File: Hooks.java
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import io.cucumber.java.Before;
+import io.cucumber.java.After;
 
-public class BrowserDriver {
-    public static WebDriver driver;
-//
-//    public static WebDriver getDriver() {
-//        if (driver == null) {
-//            driver = new ChromeDriver();  // Selenium Manager handles setup
-//        }
-//        return driver;
-//    }
-//
-//    public static void quitDriver() {
-//        if (driver != null) {
-//            driver.quit();
-//            driver = null;
-//        }
-//    }
+public class Hooks {
+    private static WebDriver driver;
+
+    @Before
+    public void setUp() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+    }
+
+    @After
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+
+    public static WebDriver getDriver() {
+        return driver;
+    }
 }
