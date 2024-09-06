c6eadea28c7d5c99e82b76204bf61f9a62c3fa5a
jmsdaq <jamesdaquioag6@gmail.com>
2024-09-06 23:39:11 +0800

    fix: update Page Object with PageFactory

--- src/test/java/pages/HomePage.java
+++ src/test/java/pages/HomePage.java
@@ -1,31 +1,36 @@
 package pages;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
-import org.openqa.selenium.By;
-import org.openqa.selenium.chrome.ChromeDriver;
-import utility.BrowserDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
+public class HomePage {
+    Webdriver driver;
 
-public class HomePage extends BrowserDriver {
-
-    public static String hamburger_menu_xpath = "//*[@id=\"menuToggle\"]/input";
-    public static String signIn_link_xpath = "//*[@id=\"menu\"]/a[2]/li";
-    public static String onlineProduct_lt = "Online Products";
-
-    public static void click_hamburger_button() throws InterruptedException {
-        WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver();
-        driver.get("https://anupdamoda.github.io/AceOnlineShoePortal/index.html");
-        Thread.sleep(2000);
-        driver.findElement(By.xpath(hamburger_menu_xpath)).click();
-    }
-    public static void click_signIn_link () throws InterruptedException {
-        Thread.sleep(2000);
-        driver.findElement(By.xpath(signIn_link_xpath)).click();
+    public HomePage(Webdriver driver) {
+        this.driver = driver;
+        PageFactory.initElements(drive, this);
     }
 
-    public static void click_onlineProducts_link () throws InterruptedException {
-        Thread.sleep(2000);
-        driver.findElement(By.linkText(onlineProduct_lt)).click();
+    @FindBy(xpath = "//*[@id=\"menuToggle\"]/input")
+    WebElement hamburger_menu;
+
+    @FindBy(xpath = "//*[@id=\"menu\"]/a[2]/li")
+    WebElement signIn_xpath;
+
+    @FindBy(linkText = "Online Products")
+    WebElement onlineProduct_lt;
+
+    public void clickHamburger() {
+        hamburger_menu.click();
     }
+
+    public void clickSignIn() {
+        signIn_xpath.click();
+    }
+
+    public void clickOnlineProd() {
+        onlineProduct_lt.click();
+    }
+
 }
