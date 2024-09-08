60866e421bc07b31280a47c8ad8288b084e597ca
jmsdaq <jamesdaquioag6@gmail.com>
2024-09-09 00:01:18 +0800

    feat: working page file

--- src/test/java/pages/HomePage.java
+++ src/test/java/pages/HomePage.java
@@ -1,36 +1,45 @@
 package pages;
 
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class HomePage {
-    Webdriver driver;
+    WebDriver driver;
 
-    public HomePage(Webdriver driver) {
+    public HomePage(WebDriver driver) {
         this.driver = driver;
-        PageFactory.initElements(drive, this);
+        PageFactory.initElements(driver, this);
     }
 
     @FindBy(xpath = "//*[@id=\"menuToggle\"]/input")
     WebElement hamburger_menu;
 
-    @FindBy(xpath = "//*[@id=\"menu\"]/a[2]/li")
-    WebElement signIn_xpath;
-
-    @FindBy(linkText = "Online Products")
+    @FindBy(xpath = "//*[@id=\"menu\"]/a[3]/li")
     WebElement onlineProduct_lt;
+
+    @FindBy(className = "formalshoedropdown")
+    WebElement formalShoe;
+
+    @FindBy(xpath = "/html/body/div[2]/table/tbody/tr[1]/td[1]")
+    WebElement productName;
 
     public void clickHamburger() {
         hamburger_menu.click();
     }
 
-    public void clickSignIn() {
-        signIn_xpath.click();
+    public void clickOnlineProd() throws InterruptedException {
+        Thread.sleep(2000);
+        onlineProduct_lt.click();
     }
 
-    public void clickOnlineProd() {
-        onlineProduct_lt.click();
+    public void clickFormalShoe() throws InterruptedException{
+        formalShoe.click();
+    }
+
+    public String getProductName() throws InterruptedException{
+        return productName.getText();
     }
 
 }
