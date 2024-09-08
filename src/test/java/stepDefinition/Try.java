88812f6415a6624058f85e73cfee3da2890f93bf
jmsdaq <jamesdaquioag6@gmail.com>
2024-09-09 00:04:10 +0800

    fix: resolve error in step file

---
+++ src/test/java/stepDefinition/Try.java
@@ -0,0 +1,40 @@
+package stepDefinition;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.junit.Assert;
+import org.openqa.selenium.WebDriver;
+import pages.HomePage;
+import utility.Hooks;
+
+
+public class Try {
+
+    WebDriver driver;
+    HomePage homePage;
+
+    public Try() {
+        this.driver = Hooks.getDriver();
+        homePage = new HomePage(driver);
+    }
+    @Given("User navigate to the the hamburger menu")
+    public void user_navigate_to_the_the_hamburger_menu() throws InterruptedException {
+        homePage.clickHamburger();
+    }
+    @When("User clicks on the online products")
+    public void user_clicks_on_the_online_products() throws InterruptedException{
+        homePage.clickOnlineProd();
+    }
+    @And("User clicks on formal shoes drop down")
+    public void user_clicks_on_formal_shoes_drop_down() throws InterruptedException {
+        homePage.clickFormalShoe();
+    }
+    @Then("User should be able to view the formal shoe products")
+    public void user_should_be_able_to_view_the_formal_shoe_products() throws InterruptedException {
+        String productName = homePage.getProductName();
+        Assert.assertEquals(productName, "   Classic Cheltenham");
+
+    }
+}
