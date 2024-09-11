36ec96eedbe879f44b2c674f88bafec3600932f4
jmsdaq <jamesdaquioag6@gmail.com>
2024-09-11 17:09:58 +0800

    fix: refactor utility functions

--- src/test/java/utility/BrowserDriver.java
+++ src/test/java/utility/BrowserDriver.java
@@ -6,23 +6,13 @@ import org.openqa.selenium.chrome.ChromeOptions;
 
 public class BrowserDriver {
     private static WebDriver driver;
-    private static final String BASE_URL = "https://anupdamoda.github.io/AceOnlineShoePortal/index.html"; // Your target website URL
 
     public static WebDriver getDriver() {
         if (driver == null) {
             ChromeOptions options = new ChromeOptions();
-            // options.addArguments("--headless");
+            options.addArguments("--headless"); // Enable headless mode for better performance
             driver = new ChromeDriver(options);
-            driver.manage().window().maximize();
-            driver.get(BASE_URL); // Navigate to the website
         }
         return driver;
-    }
-
-    public static void quitDriver() {
-        if (driver != null) {
-            driver.quit();
-            driver = null;
-        }
     }
 }
--- src/test/java/utility/Hooks.java
+++ src/test/java/utility/Hooks.java
@@ -1,17 +1,19 @@
-import io.github.bonigarcia.wdm.WebDriverManager;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import io.cucumber.java.Before;
+package utility;
+
 import io.cucumber.java.After;
+import io.cucumber.java.Before;
+import org.openqa.selenium.WebDriver;
+
 
 public class Hooks {
     private static WebDriver driver;
+    private static final String BASE_URL = "https://anupdamoda.github.io/AceOnlineShoePortal/index.html";
 
     @Before
     public void setUp() {
-        WebDriverManager.chromedriver().setup();    
-        driver = new ChromeDriver();
+        driver = BrowserDriver.getDriver();
         driver.manage().window().maximize();
+        driver.get(BASE_URL); // Navigate to the website
     }
 
     @After
@@ -24,4 +26,5 @@ public class Hooks {
     public static WebDriver getDriver() {
         return driver;
     }
+
 }
