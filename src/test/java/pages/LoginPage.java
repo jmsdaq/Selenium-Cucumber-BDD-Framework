8eef87b00fda7184ca6da7aaff674d5924201f9f
jmsdaq <jamesdaquioag6@gmail.com>
2024-09-11 22:37:47 +0800

    feat: initial commit for login feature

--- src/test/java/pages/LoginPage.java
+++ src/test/java/pages/LoginPage.java
@@ -1,32 +1,35 @@
 package pages;
 
-import org.openqa.selenium.By;
-import utility.BrowserDriver;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
-public class LoginPage extends BrowserDriver {
+public class LoginPage {
+    WebDriver driver;
 
-    public static String usernameId = "usr";
-    public static String passwordId = "pwd";
-    public static String loginButton = "//*[@id=\"second_form\"]/input";
-    public static String registerButton = "NewRegistration";
-
-
-    public static void sendkeys_username() throws InterruptedException{
-        driver.findElement(By.id(usernameId)).sendKeys("ducksyde@gmail.com");
-    }
-    public static void sendkeys_password() throws InterruptedException{
-        driver.findElement(By.id(passwordId)).sendKeys("P@assword!");
-    }
-    public static void click_login(){
-        driver.findElement(By.xpath(loginButton)).click();
-    }
-    public static void click_registration(){
-        driver.findElement(By.id(registerButton)).click();
+    public LoginPage(WebDriver driver) {
+        this.driver = driver;
+        PageFactory.initElements(driver, this);
     }
 
-//    public static void click_signIn_link () throws InterruptedException {
-//        Thread.sleep(2000);
-//        driver.findElement(By.xpath(signIn_link_xpath)).click();
-//    }
+    @FindBy(css = "input[placeholder='Username']")
+    WebElement username;
+
+    @FindBy(css = "input[placeholder='Password']")
+    WebElement password;
+
+    @FindBy(css = "button[type='submit']")
+    WebElement loginButton;
+
+    public void usernameInput(String username){
+        loginButton.sendKeys(username);
+    }
+    public void passwordInput(String password){
+        loginButton.sendKeys(password);
+    }
+
+    public void setLoginButton() {
+        loginButton.click();
+    }
 }
-
--- src/test/java/stepDefinition/Login.java
+++ src/test/java/stepDefinition/Login.java
@@ -1,20 +1,45 @@
 package stepDefinition;
 
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.openqa.selenium.WebDriver;
+import pages.LoginPage;
+import utility.Hooks;
 
 import static pages.LoginPage.*;
 
 public class Login {
-    @When("User successfully enters the log in details")
-    public void user_successfully_enters_the_log_in_details() throws InterruptedException{
-        sendkeys_username();
-        sendkeys_password();
-        click_login();
+
+    WebDriver driver;
+    LoginPage loginpage;
+
+    public Login() {
+        this.driver = Hooks.getDriver();
+        loginpage = new LoginPage(driver);
     }
 
-    @When("User clicks on new Registration button")
-    public void user_clicks_on_new_Registration_button() throws InterruptedException{
-        click_registration();
+    @Given("I am on the login page")
+    public void i_am_on_the_login_page() {
+        loginpage.passwordInput("Admin");
+        loginpage.passwordInput("admin123");
+        loginpage.setLoginButton();
     }
+    @When("I enter {string} and {string}")
+    public void i_enter_and(String string, String string2) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @When("I click the login button")
+    public void i_click_the_login_button() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Then("I should be redirected to the dashboard page")
+    public void i_should_be_redirected_to_the_dashboard_page() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
 
 }
