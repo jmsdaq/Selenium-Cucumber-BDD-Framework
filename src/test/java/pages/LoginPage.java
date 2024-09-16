e9cd2a34213f5a63471ad8e2367779335b897aac
jmsdaq <jamesdaquioag6@gmail.com>
2024-09-16 18:13:34 +0800

    fix: enable independent execution of login feature

--- src/test/java/pages/LoginPage.java
+++ src/test/java/pages/LoginPage.java
@@ -3,20 +3,24 @@ package pages;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
-import org.openqa.selenium.support.PageFactory;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
+//import org.openqa.selenium.support.PageFactory;
+//import org.openqa.selenium.support.ui.ExpectedConditions;
+//import org.openqa.selenium.support.ui.WebDriverWait;
+//
+//import java.time.Duration;
 
-import java.time.Duration;
-
-public class LoginPage {
-    WebDriver driver;
-    WebDriverWait wait;
+public class LoginPage extends BasePage{
+//    WebDriver driver;
+//    WebDriverWait wait;
+//
+//    public LoginPage(WebDriver driver) {
+//        this.driver = driver;
+//        this.wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+//        PageFactory.initElements(driver, this);
+//    }
 
     public LoginPage(WebDriver driver) {
-        this.driver = driver;
-        this.wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-        PageFactory.initElements(driver, this);
+        super(driver);
     }
 
     @FindBy(xpath = "//input[@name='username']")
@@ -25,8 +29,11 @@ public class LoginPage {
     @FindBy(xpath = "//input[@name='password']")
     WebElement enterPassword;
 
-    @FindBy(className = "oxd-input-field-error-message")
-    WebElement errorMessage;
+    @FindBy(css = ".oxd-input-field-error-message")  // Correct compound class selector
+    WebElement emptyFieldsError;
+
+    @FindBy(css = ".oxd-text.oxd-text--p.oxd-alert-content-text")  // Correct compound class selector
+    WebElement invalidCredentialsError;
 
     @FindBy(css = "button[type='submit']")
     WebElement loginButton;
@@ -38,31 +45,63 @@ public class LoginPage {
 
     public void navigateToLoginPage() {
         driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login"); // Update URL as necessary
-        wait.until(ExpectedConditions.visibilityOf(enterUsername));
     }
     public void usernameInput(String username){
+        waitForElementVisible(enterUsername);
         enterUsername.sendKeys(username);
     }
     public void passwordInput(String password){
+        waitForElementVisible(enterPassword);
         enterPassword.sendKeys(password);
     }
 
     public void setLoginButton() {
+        waitForElementClickable(loginButton);
         loginButton.click();
     }
 
-    public String getErrorMessage(){
-        wait.until(ExpectedConditions.visibilityOf(errorMessage));
-        return errorMessage.getText();
+//    public String isEmptyFieldsErrorDisplayed(){
+//        return emptyFieldsError.getText();
+//    }
+//
+//    public String isInvalidCredentialsErrorDisplayed(){
+//        return invalidCredentialsError.getText();
+//    }
+//    public boolean isEmptyFieldsErrorDisplayed() {
+//        return emptyFieldsError.isDisplayed();
+//    }
+//
+//    public boolean isInvalidCredentialsErrorDisplayed() {
+//        return invalidCredentialsError.isDisplayed();
+//    }
+//
+//    public String getErrorMessage() {
+//        if (isEmptyFieldsErrorDisplayed()) {
+//            return emptyFieldsError.getText();
+//        } else if (isInvalidCredentialsErrorDisplayed()) {
+//            return invalidCredentialsError.getText();
+//        } else {
+//            return null;
+//        }
+//    }
+    public String isInvalidCredentialsErrorDisplayed(){
+        waitForElementVisible(invalidCredentialsError);
+        return invalidCredentialsError.getText();
     }
 
-    public String getExpectedUrl(){
-        wait.until(ExpectedConditions.elementToBeClickable(loginButton));
-        return dashboardUrl;
+    public String isEmptyFieldsErrorDisplayed(){
+        waitForElementVisible(emptyFieldsError);
+        return emptyFieldsError.getText();
     }
+
+//    public String getExpectedUrl(){
+//        waitForElementVisible(loginButton);
+//        return dashboardUrl;
+//    }
 
     public String getDashboardElementText() {
-        wait.until(ExpectedConditions.visibilityOf(dashboardElement));
+//        wait.until(ExpectedConditions.visibilityOf(dashboardElement));
+        waitForElementVisible(dashboardElement);
         return dashboardElement.getText();
     }
 }
--- src/test/java/stepDefinition/Login.java
+++ src/test/java/stepDefinition/Login.java
@@ -3,43 +3,71 @@ package stepDefinition;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import io.cucumber.plugin.event.Node;
 import org.openqa.selenium.WebDriver;
 import pages.LoginPage;
 import utility.Hooks;
 
-import static pages.LoginPage.*;
+import static org.junit.Assert.*;
 
 public class Login {
 
-    WebDriver driver;
-    LoginPage loginpage;
+//    WebDriver driver;
+    private WebDriver driver = Hooks.driver;
+    private LoginPage loginpage;
+//    LoginPage loginpage;
 
-    public Login() {
-        this.driver = Hooks.getDriver();
-        loginpage = new LoginPage(driver);
-    }
+//    public Login() {
+//        this.driver = Hooks.getDriver();
+//        private LoginPage loginPage;
+//    }
 
     @Given("I am on the login page")
-    public void i_am_on_the_login_page() {
-        loginpage.passwordInput("Admin");
-        loginpage.passwordInput("admin123");
-        loginpage.setLoginButton();
+    public void i_am_on_the_login_page() throws InterruptedException{
+        loginpage = new LoginPage(driver);
+        Thread.sleep(2000);
+        loginpage.navigateToLoginPage();
     }
     @When("I enter {string} and {string}")
-    public void i_enter_and(String string, String string2) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
-    }
-    @When("I click the login button")
-    public void i_click_the_login_button() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
-    }
-    @Then("I should be redirected to the dashboard page")
-    public void i_should_be_redirected_to_the_dashboard_page() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+    public void i_enter_and(String username, String password) {
+        loginpage.usernameInput(username);
+        loginpage.passwordInput(password);
     }
 
+
+    @When("I click the login button")
+    public void i_click_the_login_button() {
+        loginpage.setLoginButton();
+    }
+
+    @Then("I should be redirected to the dashboard page")
+    public void i_should_be_redirected_to_the_dashboard_page() throws InterruptedException {
+         String actualTitle = loginpage.getDashboardElementText(); // Update with your dashboard page title
+         String expectedTitle = "Dashboard";
+         assertEquals(expectedTitle, actualTitle);
+
+        // Print dashboard element text for verification
+        String dashboardText = loginpage.getDashboardElementText();
+        System.out.println("Dashboard Text: " + dashboardText);
+
+        // Alternatively, verify the page url
+//        String expectedUrl = loginpage.getExpectedUrl();
+//        String currentUrl = driver.getCurrentUrl();
+//        assertEquals(expectedUrl, currentUrl);
+
+    }
+
+    @Then("I should see an error message {string}")
+    public void i_should_see_an_error_message(String expectedErrorMessage) {
+        String actualErrorMessage = loginpage.isInvalidCredentialsErrorDisplayed();
+//        assertNotNull(actualErrorMessage, "Error message should not be null");
+        assertEquals(expectedErrorMessage, actualErrorMessage);
+    }
+
+    @Then("I should see a message {string}")
+    public void i_should_see_a_message(String expectedMessage){
+        String actualMessage = loginpage.isEmptyFieldsErrorDisplayed();
+        assertEquals(expectedMessage, actualMessage);
+    }
 
 }
