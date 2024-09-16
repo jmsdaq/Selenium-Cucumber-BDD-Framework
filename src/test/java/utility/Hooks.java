1863b847f0c507d8b59c30dc87fe4c14b15bb7bf
jmsdaq <jamesdaquioag6@gmail.com>
2024-09-16 18:05:51 +0800

    fix: fix hook execution order

--- src/test/java/utility/Hooks.java
+++ src/test/java/utility/Hooks.java
@@ -1,19 +1,50 @@
+//package utility;
+//
+//import io.cucumber.java.After;
+//import io.cucumber.java.Before;
+//import org.openqa.selenium.WebDriver;
+//
+//
+//public class Hooks {
+//    private static WebDriver driver;
+////    private static final String BASE_URL = "https://anupdamoda.github.io/AceOnlineShoePortal/index.html";
+//
+//    @Before
+//    public void setUp() {
+//        driver = BrowserDriver.getDriver();
+//        driver.manage().window().maximize();
+////        driver.get(BASE_URL); // Navigate to the website
+//    }
+//
+//    @After
+//    public void tearDown() {
+//        if (driver != null) {
+//            driver.quit();
+//        }
+//    }
+//
+//    public static WebDriver getDriver() {
+//        return driver;
+//    }
+//
+//}
+
 package utility;
 
-import io.cucumber.java.After;
 import io.cucumber.java.Before;
+import io.cucumber.java.After;
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
-
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Hooks {
-    private static WebDriver driver;
-    private static final String BASE_URL = "https://anupdamoda.github.io/AceOnlineShoePortal/index.html";
+    public static WebDriver driver;
 
     @Before
     public void setUp() {
-        driver = BrowserDriver.getDriver();
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
         driver.manage().window().maximize();
-        driver.get(BASE_URL); // Navigate to the website
     }
 
     @After
@@ -22,9 +53,5 @@ public class Hooks {
             driver.quit();
         }
     }
-
-    public static WebDriver getDriver() {
-        return driver;
-    }
-
 }
+
