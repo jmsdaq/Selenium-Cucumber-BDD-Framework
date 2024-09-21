dfdba0d9421425aa12399f45029704652ea8ce90
jmsdaq <jamesdaquioag6@gmail.com>
2024-09-21 21:28:42 +0800

    feat: refactor test runner config

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -1,15 +1,7 @@
 package runner;
 
-import io.cucumber.junit.Cucumber;
-import io.cucumber.junit.CucumberOptions;
-import org.junit.runner.RunWith;
+import io.cucumber.junit.platform.engine.Cucumber;
 
-@RunWith(Cucumber.class)
-@CucumberOptions(
-        features = "src/test/java/features",
-        glue = {"stepDefinition", "utility"},
-        plugin = {"io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm"}
-)
-public class TestRunner{
-
+@Cucumber
+public class TestRunner {
 }
