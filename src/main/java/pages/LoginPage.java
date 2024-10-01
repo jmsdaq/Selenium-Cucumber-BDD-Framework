5678a475c75657d8eed2acb06b3fecc3ade514c7
jmsdaq <jamesdaquioag6@gmail.com>
2024-10-01 21:50:08 +0800

    feat: implement initial page model and steps with DI

--- src/main/java/pages/LoginPage.java
+++ src/main/java/pages/LoginPage.java
@@ -1,14 +1,13 @@
 package pages;
 
-import org.openqa.selenium.WebDriver;
+import base.PageContext;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 
-public class LoginPage extends BasePage{
-
-    public LoginPage(WebDriver driver) {
-        super(driver);
-    }
+public class LoginPage{
+    private final PageContext context;
 
     @FindBy(xpath = "//input[@name='username']")
     WebElement enterUsername;
@@ -16,53 +15,51 @@ public class LoginPage extends BasePage{
     @FindBy(xpath = "//input[@name='password']")
     WebElement enterPassword;
 
+    @FindBy(css = "button[type='submit']")
+    WebElement loginButton;
+
     @FindBy(css = ".oxd-input-field-error-message")  // Correct compound class selector
     WebElement emptyFieldsError;
 
     @FindBy(css = ".oxd-text.oxd-text--p.oxd-alert-content-text")  // Correct compound class selector
     WebElement invalidCredentialsError;
 
-    @FindBy(css = "button[type='submit']")
-    WebElement loginButton;
-
     @FindBy(xpath = "//h6[text()='Dashboard']")
     WebElement dashboardElement;
 
     public String dashboardUrl = "https://opensource-demo.orangehrmlive.com/web/index.php/dashboard/index";
 
-    public void navigateToLoginPage() {
-        driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login"); // Update URL as necessary
+    public LoginPage(PageContext context) {
+        this.context = context;
+        PageFactory.initElements(context.getDriver(), this);
     }
+
+    public void navigateToLoginPage() {
+        context.getDriver().get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login"); // Update URL as necessary
+    }
+
     public void usernameInput(String username){
-        waitForElementVisible(enterUsername);
-        enterUsername.sendKeys(username);
+        context.getWait().until(ExpectedConditions.visibilityOf(enterUsername)).sendKeys(username);
     }
     public void passwordInput(String password){
-        waitForElementVisible(enterPassword);
-        enterPassword.sendKeys(password);
+        context.getWait().until(ExpectedConditions.visibilityOf(enterPassword)).sendKeys(password);
     }
 
-    public void setLoginButton() {
-        waitForElementClickable(loginButton);
-        loginButton.click();
+//    public void setLogin(){
+//        context.getWait().until(ExpectedConditions.visibilityOf(enterUsername)).sendKeys(username);
+//        context.getWait().until(ExpectedConditions.visibilityOf(enterPassword)).sendKeys(password);
+//        context.getWait().until(ExpectedConditions.elementToBeClickable(loginButton)).click();
+//    }
+    public void loginButton() {
+        context.getWait().until(ExpectedConditions.elementToBeClickable(loginButton)).click();
+    }
+    public String isEmptyFieldsErrorDisplayed() {
+        return context.getWait().until(ExpectedConditions.visibilityOf(emptyFieldsError)).getText();
+    }
+    public String isInvalidCredentialsErrorDisplayed() {
+        return context.getWait().until(ExpectedConditions.visibilityOf(invalidCredentialsError)).getText();
     }
 
-    public void setLogin(String username, String password) {
-        waitForElementClickable(enterUsername);
-        usernameInput(username);
-        passwordInput(password);
-        setLoginButton();
-    }
-
-    public String isInvalidCredentialsErrorDisplayed(){
-        waitForElementVisible(invalidCredentialsError);
-        return invalidCredentialsError.getText();
-    }
-
-    public String isEmptyFieldsErrorDisplayed(){
-        waitForElementVisible(emptyFieldsError);
-        return emptyFieldsError.getText();
-    }
 
 //    public String getExpectedUrl(){
 //        waitForElementVisible(loginButton);
@@ -70,7 +67,6 @@ public class LoginPage extends BasePage{
 //    }
 
     public String getDashboardElementText() {
-        waitForElementVisible(dashboardElement);
-        return dashboardElement.getText();
+        return context.getWait().until(ExpectedConditions.visibilityOf(dashboardElement)).getText();
     }
 }
--- src/test/java/stepDefinition/Login.java
+++ src/test/java/stepDefinition/Login.java
@@ -1,53 +1,50 @@
 package stepDefinition;
 
+import base.PageContext;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import io.cucumber.plugin.event.Node;
 import org.openqa.selenium.WebDriver;
 import pages.LoginPage;
-import utility.Hooks;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class Login {
+    private final PageContext context;
+    private final LoginPage loginPage;
 
-//    WebDriver driver;
-    private WebDriver driver = Hooks.driver;
-    private LoginPage loginpage;
-//    LoginPage loginpage;
+    public Login(PageContext context) {
+        this.context = context;
+        this.loginPage = new LoginPage(context);
+    }
 
-//    public Login() {
-//        this.driver = Hooks.getDriver();
-//        private LoginPage loginPage;
-//    }
 
     @Given("I am on the login page")
-    public void i_am_on_the_login_page() throws InterruptedException{
-        loginpage = new LoginPage(driver);
-        loginpage.navigateToLoginPage();
-        Thread.sleep(2000); // Wait for the page to load
+    public void i_am_on_the_login_page() throws InterruptedException {
+        loginPage.navigateToLoginPage();
     }
     @When("I enter {string} and {string}")
     public void i_enter_and(String username, String password) {
-        loginpage.usernameInput(username);
-        loginpage.passwordInput(password);
+        loginPage.usernameInput(username);
+        loginPage.passwordInput(password);
     }
-
 
     @When("I click the login button")
     public void i_click_the_login_button() {
-        loginpage.setLoginButton();
+        loginPage.loginButton();
     }
+
+
 
     @Then("I should be redirected to the dashboard page")
     public void i_should_be_redirected_to_the_dashboard_page() {
-         String actualTitle = loginpage.getDashboardElementText(); // Update with your dashboard page title
+         String actualTitle = loginPage.getDashboardElementText(); // Update with your dashboard page title
          String expectedTitle = "Dashboard";
          assertEquals(expectedTitle, actualTitle);
 
         // Print dashboard element text for verification
-        String dashboardText = loginpage.getDashboardElementText();
+        String dashboardText = loginPage.getDashboardElementText();
         System.out.println("Dashboard Text: " + dashboardText);
 
         // Alternatively, verify the page url
@@ -59,14 +56,14 @@ public class Login {
 
     @Then("I should see an error message {string}")
     public void i_should_see_an_error_message(String expectedErrorMessage) {
-        String actualErrorMessage = loginpage.isInvalidCredentialsErrorDisplayed();
+        String actualErrorMessage = loginPage.isInvalidCredentialsErrorDisplayed();
 //        assertNotNull(actualErrorMessage, "Error message should not be null");
         assertEquals(expectedErrorMessage, actualErrorMessage);
     }
 
     @Then("I should see a message {string}")
     public void i_should_see_a_message(String expectedMessage){
-        String actualMessage = loginpage.isEmptyFieldsErrorDisplayed();
+        String actualMessage = loginPage.isEmptyFieldsErrorDisplayed();
         assertEquals(expectedMessage, actualMessage);
     }
 
